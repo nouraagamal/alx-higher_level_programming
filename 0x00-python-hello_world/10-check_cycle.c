6a6dc154aa25509bde2420ba258d924a1ad6ac5e
nouraagamal <nouragamal011@gmail.com>
2023-10-30 16:21:07 -0700

    dn

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function to check if a list is cycle or not
+ * list: pointer
+ * Return: 1 or 0 ow
+*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *s = list, *f = list;
+
+	while (f && f.next)
+	{
+		s = s.next;
+		f = f.next.next;
+		if (f == s)
+			return (1);
+	}
+	return (0);
+}
