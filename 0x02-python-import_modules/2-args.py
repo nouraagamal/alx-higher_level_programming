6fb1a2ef8f1a201d955d4de45fb23e524be80276
nouraagamal <nouragamal011@gmail.com>
2023-11-02 15:21:18 -0700

    .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+
     c = len(sys.argv) - 1
+
     if c == 0:
         print("0 arguments.")
     elif c == 1:
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+
     s = 0
+
     for i in range(len(sy.argv) - 1):
         s += int(sys.argv[i++])
     print(s)
