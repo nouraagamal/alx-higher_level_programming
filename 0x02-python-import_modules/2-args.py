f3d980bbfb4d19510e683b0492933c20e6083b98
nouraagamal <nouragamal011@gmail.com>
2023-11-02 15:32:46 -0700

    .

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if c == 0:
         print("0 arguments.")
     elif c == 1:
-        print("1 arguments.")
+        print("1 argument:")
     else:
         print("{} arguments:".format(c))
     for i in range (c):
