aa851f6f9d47d062042af20c95b26ccd73131cf9
nouraagamal <nouragamal011@gmail.com>
2023-11-07 14:34:45 -0800

    .

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     for i in range(len(my_string)):
         if (my_string[i] != 'c' and my_string[i] != 'C'):
-            x = "" my_string[i]
+            x = "" + my_string[i]
     return x
