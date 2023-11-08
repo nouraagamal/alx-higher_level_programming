458d6a200c2e1b60b6646cfe86ffe01509863250
nouraagamal <nouragamal011@gmail.com>
2023-11-08 07:15:00 -0800

    .

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     s = 0
-    for i in range(1, len(sy.argv)):
+    for i in range(1, len(sys.argv)):
         s += int(sys.argv[i])
     print("{}".format(s))
