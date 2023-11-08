2dcacdba1d79920698acbf3e58efd862c2f10250
nouraagamal <nouragamal011@gmail.com>
2023-11-08 06:42:32 -0800

    .

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {key: 2 * a_dictionary[key] for key in a_dictionary.keys()}
+    return({k: 2 * a_dictionary[k] for k in a_dictionary.keys()})
