2cec66de2ce49a8e08ef9a72c1ea416321eb2077
nouraagamal <nouragamal011@gmail.com>
2023-11-12 16:06:10 -0800

    .

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,12 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
     for x in matrix:
+    {
         if len(x) == 0:
             print()
         for i in range(len(x)):
+        {
             print("{:d}".format(x),
             end = "\n" if i is len(x) - 1 else " ")
+        }
+    }
