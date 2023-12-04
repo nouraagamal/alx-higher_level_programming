ab7a2ad1e6ef215af473f823ad631ac74cfa9e72
nouraagamal <nouragamal011@gmail.com>
2023-12-04 03:47:16 -0800

    .

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,9 +6,13 @@
 class Rectangle:
     '''comment'''
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
