3d93e5b80afc6b651205af8d1ef1307736cf6acd
nouraagamal <nouragamal011@gmail.com>
2024-02-13 16:00:37 -0800

    ....

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Uses an array to loop through and print strings
+
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
