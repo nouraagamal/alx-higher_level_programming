ee8a9f9ce301b692dc77b9e2a146626af342274f
nouraagamal <nouragamal011@gmail.com>
2024-03-27 17:08:30 -0700

    ..

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Bash script thats displays the size of the body of the response.
+
+curl -s "$1" | wc -c
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Bash script that takes in a URL, and displays the body of the response.
+
+curl -sL "$1"
