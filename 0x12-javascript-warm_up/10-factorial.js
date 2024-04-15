4dbe01240d8ca7f2442e9b82de790e7239415211
nouraagamal <nouragamal011@gmail.com>
2024-04-15 14:38:47 -0700

    .

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,10 +3,10 @@
 
 function factorial (n) {
   if (n === 1) {
-	  return (1);
+	  return 1;
   }
 
-  return (n * factorial(n - 1));
+  return n * factorial(n - 1);
 }
 
 if (isNaN(process.argv[2])) {
