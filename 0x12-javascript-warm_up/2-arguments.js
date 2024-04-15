c3758c50116268ebecbe716e0c732bd9ad94e57a
nouraagamal <nouragamal011@gmail.com>
2024-04-15 13:42:15 -0700

    ..

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 // prints a message depending of the number of arguments passed
-
 const count = process.argv.length;
 console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
