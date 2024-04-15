746b6344bd4cf5d725f4c053ac14f61e019ad88f
nouraagamal <nouragamal011@gmail.com>
2024-04-15 15:51:14 -0700

    .

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-//searches the second biggest integer in the list of arguments.
+// searches the second biggest integer in the list of arguments.
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const args = [];
+
   for (let i = 2; i < process.argv.length; i++) {
     args[i - 2] = process.argv[i];
   }
