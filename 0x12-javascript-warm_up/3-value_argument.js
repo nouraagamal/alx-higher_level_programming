459d81e135f7a9e43992a043d5011d8507fc3b1c
nouraagamal <nouragamal011@gmail.com>
2024-04-15 13:27:10 -0700

    ..

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
 //prints the first argument passed to it
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+
+console.log(process.argv[2] === undefined ? 'No argument' : process.argv[2]);
