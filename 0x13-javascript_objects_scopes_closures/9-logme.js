4b46b42754304cbe4a8a24c0867c75fd6e30078e
nouraagamal <nouragamal011@gmail.com>
2024-04-16 14:22:58 -0700

    ...

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// prints the number of arguments already printed and the new argument value
+let num = 0;
+exports.logMe = function (item) {
+  console.log(`${num++}: ${item}`)
+};
