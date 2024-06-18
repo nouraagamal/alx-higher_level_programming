de2a1ee6633815f5dda7139ab1240d7a94a10209
nouraagamal <nouragamal011@gmail.com>
2024-06-18 16:35:54 -0700

    ..

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL
+displays the body of the response (decoded in utf-8).
+manage urllib.error.HTTPError exceptions and print: Error code:
+followed by the HTTP status code
+"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("utf8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
