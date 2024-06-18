0f3320772f413819a442320bf9646450d29bd418
nouraagamal <nouragamal011@gmail.com>
2024-06-18 15:59:06 -0700

    ..

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL.
-displays the value of the X-Request-Id variable found in the header of the response.
+"""Python script takes in a URL, sends a request to the URL.
+displays the value of X-Request-Id variable found in header of response.
 """
 import sys
 import urllib.request
