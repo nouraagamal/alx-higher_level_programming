c0587acfcb5f20dbaf2b150e54828fc2dcc50d35
nouraagamal <nouragamal011@gmail.com>
2024-06-18 17:14:46 -0700

    ..

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,10 +8,10 @@ import requests
 
 
 if __name__ == "__main__":
-    if len(argv) < 2:
+    if len(sys.argv) < 2:
         q = ""
     else:
-        q = argv[1]
+        q = sys.argv[1]
     payload = {'q': q}
     url = "http://0.0.0.0:5000/search_user"
     r = requests.post(url, data=payload)
