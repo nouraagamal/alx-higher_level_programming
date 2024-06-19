b48818752476f33a66b9fd0e860f94d357cd62bd
nouraagamal <nouragamal011@gmail.com>
2024-06-18 17:09:18 -0700

    ...

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a Python script that takes in a letter and sends a POST
+request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        q = ""
+    else:
+        q = argv[1]
+    payload = {'q': q}
+    url = "http://0.0.0.0:5000/search_user"
+    r = requests.post(url, data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
