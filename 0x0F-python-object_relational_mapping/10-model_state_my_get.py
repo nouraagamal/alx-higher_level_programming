05d2a42ce24db9a039d9f93dee0fabcc6f18ba78
nouraagamal <nouragamal011@gmail.com>
2024-06-25 10:14:23 -0700

    .

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,15 +10,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
+
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+        argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.create_all(engine)
-
     s_tate = session.query(State).filter(State.name == argv[4]).first()
-
     if s_tate:
         print("{}".format(s_tate.id))
     else:
